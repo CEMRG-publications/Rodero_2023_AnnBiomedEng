f845ecc5c3f82c6b6f19d0c1a1372c3c7207a7aa
Cristobal Rodero <cristobal.rodero_gomez@kcl.ac.uk>
2021-03-31 11:41:04 +0100

    Functions to write a pts file from a vtx file

--- python/files_manipulations.py
+++ python/files_manipulations.py
@@ -45,3 +45,43 @@ class surf:
                                             axis = 0))
         
         return vtxvec
+
+class pts:
+    
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.size = p1.shape[0]
+
+    @classmethod
+    def read(cls, pathname):
+    
+        ptsfile = np.genfromtxt(pathname, delimiter = ' ',
+                                    dtype = float, skip_header = 1
+                                    )
+
+        p1 = ptsfile[:,0]
+        p2 = ptsfile[:,1]
+        p3 = ptsfile[:,2]
+
+        return cls(p1, p2, p3)
+
+    def extract(self,vtx_array):
+        
+        new_p1 = self.p1[vtx_array]
+        new_p2 = self.p2[vtx_array]
+        new_p3 = self.p3[vtx_array]
+
+        return pts(new_p1, new_p2, new_p3)
+
+    def write(self,pathname):
+
+        header = np.array([str(self.size)])
+        data = np.array([self.p1, self.p2, self.p3])
+
+        np.savetxt(pathname, header, fmt='%s')
+
+        with open(pathname, "ab") as datafile_id:
+            datafile_id.write(b"\n")
+            np.savetxt(datafile_id, np.transpose(data), fmt = "%s")
--- python/main.py
+++ python/main.py
@@ -3,7 +3,7 @@
 import os
 
 import prepare_mesh 
-import files_manipulations as fm
+import files_manipulations
 import fibres
 
 def main():
@@ -15,9 +15,13 @@ def main():
     # prepare_mesh.unzip_meshes(bundle_number)
     # prepare_mesh.vtk_mm2carp_um(heart)
     # prepare_mesh.extract_bdry_bayer(heart)
-    prepare_mesh.map_biv(heart)
-    fibres.run_laplacian(heart)
-    fibres.rb_bayer(heart)
+    # prepare_mesh.map_biv(heart)
+    # fibres.run_laplacian(heart)
+    # fibres.rb_bayer(heart)
 
+    dummy_pts = files_manipulations.pts.read("/data/fitting/Full_Heart_Mesh_1/dummy.pts")
+    dummy_vtx = files_manipulations.read_vtx("/data/fitting/Full_Heart_Mesh_1/dummy.vtx")
+    new_pts = files_manipulations.pts.extract(dummy_pts,dummy_vtx)
+    new_pts.write("/data/fitting/Full_Heart_Mesh_1/new_pts.pts")
 if __name__ == "__main__":
     main()
