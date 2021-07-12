c750746dba82f78d290efae49a21ad8986821bab
Cristobal Rodero <cristobal.rodero_gomez@kcl.ac.uk>
2021-07-12 09:56:51 +0100

    Added documentation to run_new_wave

--- python/fitting_hm.py
+++ python/fitting_hm.py
@@ -289,6 +289,32 @@ def run_new_wave(num_wave = 3, run_simulations = False, train_GPE = False,
                 fill_wave_space = False, cutoff = 2.0, n_samples = 150,
                 generate_simul_pts = 10, subfolder = ".", training_set_memory = 100,
                 skip_filtering = True):
+    """Pipeline to run a new wave for only EP in the template mesh.
+
+    Args:
+        num_wave (int, optional):  Wave number (establishes the folder name). 
+        Defaults to 3.
+        run_simulations (bool, optional): If True, runs the simulations.
+        Otherwise it loads them. Defaults to False.
+        train_GPE (bool, optional): If True, trains the emulators. Otherwise it
+        loads them. Defaults to False.
+        fill_wave_space (bool, optional): If True, generates the new points in 
+        the NROY. Defaults to False.
+        cutoff (float, optional): Implausibility threshold. Defaults to 2.0.
+        n_samples (int, optional): Number of points to use in train/validation.
+        Validation will be 20% of that value, and the 80% remaining will be
+        split in 80%/20% for training/test. Defaults to 150.
+        generate_simul_pts (int, optional): Number of points to generate as
+        simulation input for the next wave. Defaults to 10.
+        subfolder (str, optional):  Name of the subfolder in /data/fitting to
+        work on. Defaults to ".".
+        training_set_memory (int, optional):  The training set of the current 
+        emulator will be expanded with the previous training_set_memory training
+        sets. Defaults to 100.
+        skip_filtering (bool, optional): If False, filters the simulations and 
+        emulations that are out of a specific range (5SD of the mean). Defaults 
+        to True.
+    """
 
     #==========================================================================
     # For each wave i, with a implausibility threshold cutoff_i and a maximum
