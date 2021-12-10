d668f738b91fd00d50f4f4134afe99daa5451e93
Cristobal Rodero Gomez <cristobal.rodero_gomez@kcl.ac.uk>
2021-12-10 10:31:46 +0000

    Changed the name of the temporary folders when creating the atlas to allowed more parallelization

--- python/generate_meshes.py
+++ python/generate_meshes.py
@@ -22,8 +22,8 @@ def deformetrica_single_mesh(mesh_path, anatomy_values, i):
     temp_base_name = anatomy_values[i].replace(",", "")[:-36]
     csv_file_name = os.path.join(PROJECT_PATH, "meshes", "meshing_files", temp_base_name + ".csv")
     os.system("python3.6 " + os.path.join("/home", "crg17", "Desktop", "KCL_projects", "fitting", "python",
-                                          "CardiacMeshConstruction_" + str(i),
-                                          "pipeline.py ") + csv_file_name + " " + mesh_path + " " + str(i))
+                                          "CardiacMeshConstruction_" + temp_base_name.replace('.',''),
+                                          "pipeline.py ") + csv_file_name + " " + mesh_path + " " + temp_base_name.replace('.',''))
 
 def preprocess_for_carp(mesh_path, anatomy_values, i):
     """Function to convert to carp format, convert the pts to micrometre instead of millimetre and safe-copying the
@@ -43,7 +43,7 @@ def preprocess_for_carp(mesh_path, anatomy_values, i):
 
     pathlib.Path(mesh_dir).mkdir(parents=True, exist_ok=True)
 
-    os.system("meshtool convert -imsh=" + os.path.join(mesh_path, "wave" + str(i) + ".vtk") +
+    os.system("meshtool convert -imsh=" + os.path.join(mesh_path, "wave" + anatomy_values[i].replace(",", "")[:-36].replace('.','') + ".vtk") +
           " -omsh=" + os.path.join(mesh_dir, mesh_name) +
           " -ifmt=vtk -ofmt=carp_txt")
 
@@ -59,7 +59,7 @@ def preprocess_for_carp(mesh_path, anatomy_values, i):
     shutil.copy(os.path.join(mesh_dir, mesh_name + ".elem"),
                 os.path.join(mesh_dir, mesh_name + "_default.elem"))
 
-    os.system("rm " + os.path.join(mesh_path, "wave" + str(i) + ".vtk"))
+    os.system("rm " + os.path.join(mesh_path, "wave" + anatomy_values[i].replace(",", "")[:-36].replace('.','') + ".vtk"))
 
 def sample_atlas(subfolder = "initial_sweep", csv_filename = "input_anatomy_training.csv"):
     """Function to generate meshes from the atlas, in parallel (in chunks of 18 meshes).
@@ -98,15 +98,16 @@ def sample_atlas(subfolder = "initial_sweep", csv_filename = "input_anatomy_trai
             np.savetxt(os.path.join(mesh_path, "meshing_files", individual_name + ".csv"),
                        np.array([header_line, sub_dataset[i]]), fmt='%s', delimiter='\n')
             os.system("rm -rf " + os.path.join("/home", "crg17", "Desktop", "KCL_projects", "fitting", "python",
-                                               "CardiacMeshConstruction_" + str(i)))
+                                               "CardiacMeshConstruction_" + individual_name.replace('.','')))
             os.system("cp -rf " + os.path.join("/home", "crg17", "Desktop", "KCL_projects", "fitting", "python",
                                                "CardiacMeshConstruction_outside ") + \
                       os.path.join("/home", "crg17", "Desktop", "KCL_projects", "fitting", "python",
-                                   "CardiacMeshConstruction_" + str(i)))
+                                   "CardiacMeshConstruction_" +individual_name.replace('.','')))
 
         Parallel(n_jobs=20)(delayed(deformetrica_single_mesh)(mesh_path,sub_dataset,i) for i in range(len(sub_dataset)))
 
         for i in range(len(sub_dataset)):
-            os.system("rm -rf " + os.path.join("/home","crg17","Desktop","KCL_projects","fitting","python","CardiacMeshConstruction_" + str(i)))
+            individual_name = sub_dataset[i].replace(",", "")[:-36]
+            os.system("rm -rf " + os.path.join("/home","crg17","Desktop","KCL_projects","fitting","python","CardiacMeshConstruction_" + individual_name.replace('.','')))
             preprocess_for_carp(mesh_path=mesh_path, anatomy_values=sub_dataset, i=i)
 
