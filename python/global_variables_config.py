4b3520b3cf332d7ade117a6f92800b530aef5633
Cristobal Rodero Gomez <cristobal.rodero_gomez@kcl.ac.uk>
2022-03-15 15:22:00 +0000

    Modified PROJECT_PATH and reversed

--- python/global_variables_config.py
+++ python/global_variables_config.py
@@ -1,7 +1,7 @@
 # PROJECT_PATH = "/data/fitting"
 PROJECT_PATH_NOSLASHES = "/media/crg17/Seagate Expansion Drive"
 # PROJECT_PATH_SLASHES = "/media/crg17/Seagate\ Expansion\ Drive"
-PROJECT_PATH = "/media/crg17/Seagate\ Expansion\ Drive"
+PROJECT_PATH = "/media/crg17/SeagateExpansionDrive"
 MESHES_ROOT_CREATE_NOSLASHES = "/media/crg17/Seagate Backup Plus"
 MESHES_ROOT_CREATE_SLASHES = "/media/crg17/Seagate\ Backup\ Plus"
 MESHES_ROOT_CHECK = "/data/fitting"
