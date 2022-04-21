80251261d781b8d25b80e5f270a9d992988b12d8
Cristóbal Rodero Gómez <cristobal.rodero_gomez@kcl.ac.uk>
2022-04-21 16:35:46 +0100

    Commented lines

--- python/main.py
+++ python/main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-import fitting_hm
+import pipeline_bhm
 
 if __name__ == "__main__":
-    fitting_hm.mechanics_prepare_simulations(num_wave=0, n_samples=34, subfolder="mechanics")
+
+    pipeline_bhm.literature_convergence(perc_convergence=95.)
+    # pipeline_bhm.patient_convergence(patient_number=1, perc_convergence=95., fixed_sd=10)
+    # pipeline_bhm.patient_convergence(patient_number=1, perc_convergence=95., fixed_sd=5)
+    # pipeline_bhm.patient_convergence(patient_number=1, perc_convergence=95., fixed_sd=1)
+    pipeline_bhm.patient_convergence(patient_number=2, perc_convergence=95., fixed_sd=10)
+    pipeline_bhm.patient_convergence(patient_number=10, perc_convergence=95., fixed_sd=10)
+    pipeline_bhm.patient_convergence(patient_number=18, perc_convergence=95., fixed_sd=10)
+    # pipeline_bhm.patient_convergence(patient_number=18, perc_convergence=95., fixed_sd=5)
+    # pipeline_bhm.patient_convergence(patient_number=18, perc_convergence=95., fixed_sd=1)
